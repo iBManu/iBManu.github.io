4be17250fca4d08c02189b4cec862b961ee1a08e
Manuel Camacho Campos <70716864+iBManu@users.noreply.github.com>
2026-07-14 14:55:18 +0200

    Delete old directory

--- old/scripts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contact-button").addEventListener("click",function() {
-    var posicionDiv = document.getElementById("contactme").offsetTop;
-
-    window.scrollTo({top: posicionDiv, behavior: "smooth"});
-});
